38696339486b85a049f18395d45b741234062806
Issa Fikadu <memiwo@gmail.com>
2017-01-13 13:26:37 -0800

    Initial project setup

---
+++ src/main/java/com/aim/AimDemoApplication.java
@@ -0,0 +1,12 @@
+package com.aim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AimDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AimDemoApplication.class, args);
+	}
+}
