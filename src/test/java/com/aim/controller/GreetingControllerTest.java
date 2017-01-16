e8261452290abca5a0ed00443a1bad73d4f170e6
Issa Fikadu <memiwo@gmail.com>
2017-01-15 16:57:36 -0800

    Adds test for GreetingsController

--- src/test/java/com/aim/controller/GreetingControllerTest.java
+++ src/test/java/com/aim/controller/GreetingControllerTest.java
@@ -1,5 +1,46 @@
 package com.aim.controller;
 
-public class GreetingControllerTest {
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.mockito.BDDMockito.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import com.aim.service.GreetingService;
 
+@RunWith(SpringRunner.class)
+@WebMvcTest(GreetingController.class)
+public class GreetingControllerTest {
+	
+	@Autowired
+	private MockMvc mvc;
+	
+	@MockBean
+	private GreetingService greetingService;
+
+	@Test
+	public void testGreeting() throws Exception{
+		given(this.greetingService.getMessage("Issa")).
+		willReturn("Hello, Issa");
+		
+		this.mvc.
+		perform(get("/greetings?name=Issa")).
+		andExpect(status().isOk()).
+		andExpect(content().string("Hello, Issa"));
+	}
+	
+	@Test
+	public void testGreetingWithLocale() throws Exception{
+		given(this.greetingService.getMessage("Issa")).
+		willReturn("Hola, Issa");
+		
+		this.mvc.
+		perform(get("/greetings?name=Issa&lang=es")).
+		andExpect(status().isOk()).
+		andExpect(content().string("Hola, Issa"));
+	}
 }
