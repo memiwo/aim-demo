96243c5417d0571cb1aa8df35989a28ea3a81762
Issa Fikadu <memiwo@gmail.com>
2017-01-15 23:20:52 -0800

    Add integration tests

--- src/test/java/com/aim/AimDemoApplicationTests.java
+++ src/test/java/com/aim/AimDemoApplicationTests.java
@@ -2,15 +2,25 @@ package com.aim;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired; 	
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = AimDemoApplication.class)
 public class AimDemoApplicationTests {
+	
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
-	public void contextLoads() {
+	public void testGreetingDefault() {
+		this.restTemplate.
+		getForEntity("/greetings",String.class).
+		getBody().
+		contentEquals("Hello jjj,");
 	}
 
 }
