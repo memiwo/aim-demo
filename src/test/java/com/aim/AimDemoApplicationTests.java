202ef07537aa3bc93d20ef9dd44961c7949c5670
Issa Fikadu <memiwo@gmail.com>
2017-01-15 23:44:06 -0800

    Added Integeration tests

--- src/test/java/com/aim/AimDemoApplicationTests.java
+++ src/test/java/com/aim/AimDemoApplicationTests.java
@@ -1,15 +1,18 @@
 package com.aim;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired; 	
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = AimDemoApplication.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class AimDemoApplicationTests {
 	
 	@Autowired
@@ -17,10 +20,22 @@ public class AimDemoApplicationTests {
 
 	@Test
 	public void testGreetingDefault() {
-		this.restTemplate.
-		getForEntity("/greetings",String.class).
-		getBody().
-		contentEquals("Hello jjj,");
+		ResponseEntity<String> response = this.restTemplate.getForEntity("/greetings",String.class);
+		assertEquals("Hello, ", response.getBody());
+	}
+	
+	@Test
+	public void testGreetingWithNameAndDefaultLocale(){
+		ResponseEntity<String> response = this.restTemplate.getForEntity("/greetings?name=John", String.class);
+		assertEquals("Hello, John", response.getBody());
+	}
+	
+	
+	@Test
+	public void testGreetingWithNameAndLocale(){
+		ResponseEntity<String> response = this.restTemplate.getForEntity("/greetings?lang=es&name=Clare", String.class);
+		
+		assertEquals("Hola, Clare", response.getBody());
 	}
 
 }
